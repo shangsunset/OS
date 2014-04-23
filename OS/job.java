a31fa205679c8c8cef03044444e83cebeb660062
Manuel Leung Wu <manuelleung93@gmail.com>
2014-04-23 16:38:50 -0400

    2nd version, i have gotten the 1st job to terminate normally. still needs a lot of work on all functions, they only have the least needed for the 1st job to finish

--- OS/job.java
+++ OS/job.java
@@ -1,29 +1,27 @@
+import java.util.Date;
 
-
-public class job  {
-	
+public class job {
 	private int jobNum;
 	private int jobPriority;
 	private int jobSize;
 	private int CpuTime;
 	private int currentTime;
 	private int jobAddress;
-   
+
 	public job() {}
 	
-   public job(int address, int p[]) {
-      jobAddress = address;
-      jobNum = p[1];
-      jobPriority = p[2];
-      jobSize = p[3];
-      CpuTime = p[4];
-      currentTime = p[5];
-   }
-
+   	public job(int address, int p[]) {
+      		jobAddress = address;
+      		jobNum = p[1];
+      		jobPriority = p[2];
+      		jobSize = p[3];
+      		CpuTime = p[4];
+      		currentTime = 0;
+   	}
 	
-   public int getJobAddress() {
-         return jobAddress;
-   }
+   	public int getJobAddress() {
+        	return jobAddress;
+   	}
 	
 	public int getJobNum() {
 		return jobNum;
@@ -46,7 +44,7 @@ public class job  {
 	public int getCpuTime() {
 		return CpuTime;
 	}
-	public void setCPUTime(int CpuTime) {
+	public void setCpuTime(int CpuTime) {
 		this.CpuTime = CpuTime;
 	}
 	public int getCurrentTime() {
@@ -55,24 +53,7 @@ public class job  {
 	public void setCurrentTime(int currentTime) {
 		this.currentTime = currentTime;
 	}
-
-    //for comparing each job's current time when using FCFS
-//    @Override
-//    public int compareTo(job other) {
-//
-//        int comparedCurrentTime = other.getCurrentTime();
-//
-//        if (this.getCurrentTime() > comparedCurrentTime) {
-//            return 1;
-//        }
-//        else if (this.getCurrentTime() < comparedCurrentTime){
-//            return -1;
-//
-//        }
-//        else
-//            return 0;
-//
-//
-//    }
-	
+	public void updateCurrentTime(int time, int timeSpentCpu) {
+		currentTime = currentTime + (time-timeSpentCpu);
+	}	
 }
--- OS/os.java
+++ OS/os.java
@@ -1,107 +1,164 @@
 import java.util.*;
 
-
-class os {
-	
+class os {	
 	/* INITIALIZATION */
 	public static LinkedList<job> jobTable;
-    public static int endPointer; //points to the last address for each job
-                                 //or 0 for 1st job in
-    public static int current; //keeps track of the current job at job table
-
+    	public static int endPointer; //points to the last address for each job
+    	public static int current; //keeps track of the current job at job table
+	public static int timeSpentCpu; //keeps track of cpu running time
+	public static boolean CpuIdling; //flag to know if cpu is in use or not
+   
 	public static void startup() {
-	     jobTable = new LinkedList<job>(); //Initialize job table
-         endPointer = 0;  //Start at the beginning
-         current = 0; //no jobs at the moment start at 0
+	  	//sos.ontrace();
+		jobTable = new LinkedList<job>(); //init job table
+      		endPointer = 0; //start at the beginning
+      		current = 0; //no jobs at the moment start at 0
+	  	timeSpentCpu = 0; //init to 0
+	  	CpuIdling = false; //at the start cpu is idling
 	}
 
 	/*	
 	public static void siodisk(int jobnum);
-
 	public static void siodrum(int jobnum, int jobsize, int coreaddress, int direction);
-
-	void ontrace();
-
-	void offtrace();
 	*/
 
 	/* INTERRUPT HANDLERS */
 
 	/* INDICATES ARRIVAL OF A NEW JOB ON THE DRUM */
 	/* SAVE INFORMATION ABOUT NEW COMING JOB */
-
 	public static void Crint(int[] a, int[] p) {
-   
-   //At the moment the crint only accepts a job and
-   //no checking wether its full, busy, etc
-   //Just trying to get 1 job running first
-   
-      //printing everything for debugging purposes
-	  System.out.println("Job Arriving: number " +p[1]+ ", priority "+
-      p[2]+", size "+p[3]+", cputime "+p[4]+", currenttime "+p[5]);
+      		//printing everything for debugging purposes
+		System.out.println("Job Arriving: number " + p[1] + ", priority " +
+      		p[2] + ", size " + p[3] + ", cputime " + p[4] + ", currenttime " +p[5]);
 		
-      //storing theinformation of the new jobs into jobTable
-		job newJob = new job(endPointer, p);
-		jobTable.add(newJob);
+      		//storing the information of the new jobs into jobTable
+	  	job newJob = new job(endPointer, p);
+	  	jobTable.add(newJob);
+	  
+      		//place job into memory by calling sos (In core)
+      		sos.siodrum(p[1], p[3], endPointer, 0);
+      		System.out.println("Job now in address, " + endPointer);
       
-      //place job into memory by calling sos (In core)
-      sos.siodrum(p[1], p[3], endPointer, 0);
-      System.out.println("Job now in address, " + endPointer);
-      
-      //keeping track of the ending of each job in memory
-      endPointer = endPointer+p[3];
-      System.out.println("ending in address, "+ endPointer);
-      
+      		//keeping track of the ending of each job in memory
+      		endPointer = endPointer + p[3];
+      		System.out.println("ending in address, " + endPointer);
+
+	  	//only dealing with 1 job at a time, so cpu is idle
+	  	//at the moment
+	  	CpuIdling = true;
 	}
    
-   // I put a print on all interrupts to know where the job is at
-   
-   public static void FirstComeFirstServe(int[] a, int[] p) {
+	//printing on all interrupts to know where the job is at
+	
+	/* SCHEDULER */
+	public static void FirstComeFirstServe(int[] a, int[] p) {
+	  	System.out.println("Currently at FCFS ");
+	  	System.out.println("CurrentTime = " + jobTable.get(current).getCurrentTime());
+	
+      		//if there is a job to run, set a to 2
+      		a[0] = 2;
+      		//put information about job
+      		//current job address to update
+      		p[2] = jobTable.get(current).getJobAddress();
+      		//current job size to p[3]
+      		p[3] = jobTable.get(current).getJobSize();
+      		//current job time slice to p[4] substract the time it runs to the
+	  	//maximum CPU time it requires
+      		p[4] = jobTable.get(current).getCpuTime()-jobTable.get(current).getCurrentTime();
+	  
+	  	//job will be going into the CPU so we set out idling flag to false
+	  	CpuIdling = false;
+	  	//we use a variable to keep track of the time spent using cpu
+	  	timeSpentCpu = p[5];  
+	}
 
-      System.out.println("Currently at FCFS ");
-
-
-       //if there is a job to run, set a to 2
-      a[0] = 2;
-      //put information about job
-      //current job address to p[2]
-      p[2] = jobTable.get(current).getJobAddress();
-      //current job size to p[3]
-      p[3] = jobTable.get(current).getJobSize();
-      //current job time slice to p[4]
-      p[4] = jobTable.get(current).getCpuTime();
-
-   }
-      
    
 	/* DISK INTERRUPT */
 	public static void Dskint(int[] a, int[] p) {
-     System.out.println("Currently at Dskint "); 
+      		System.out.println("Currently at Dskint "); 
+	 
+	  	//if our idling flag is false we want to substract 
+	  	//time spent using cpu to the current time and add it
+	  	//to the job's running time
+	  	//(this is how we keep track of quantum of time)
+	  	if(CpuIdling == false) {
+			jobTable.get(current).updateCurrentTime(p[5], timeSpentCpu);
+			System.out.println("CurrentTime = " + jobTable.get(current).getCurrentTime());
+		}
+	  
+	  	//go back to schedule the job to run
+	  	FirstComeFirstServe(a, p);
 	}
 
 	/* DRUM INTERRUPT */
 	public static void Drmint(int[] a, int[] p) {
-	  System.out.println("Currently at Drmint "); 
-     //after crint the sos calls drmint so i am just sending it
-     //back to scheduler to keep testing
-        FirstComeFirstServe(a, p);
+	  	System.out.println("Currently at Drmint "); 
+	  
+	  	//if our idling flag is false we want to substract 
+	  	//time spent using cpu to the current time and add it
+	  	//to the job's running time
+	  	//(this is how we keep track of quantum of time)
+	  	if(CpuIdling == false) {
+			jobTable.get(current).updateCurrentTime(p[5], timeSpentCpu);
+			System.out.println("CurrentTime = " + jobTable.get(current).getCurrentTime());
+		}
+	  
+	  	//go back to schedule the job to run
+      		FirstComeFirstServe(a, p);
 	}
 	
 	/* TIMER-RUN-OUT */
 	public static void Tro(int[] a, int[] p) {
-	  System.out.println("Currently at Tro ");  
+	  	System.out.println("Currently at Tro ");
+	  
+	  	//if our idling flag is false we want to substract 
+	  	//time spent using cpu to the current time and add it
+	  	//to the job's running time
+	  	//(this is how we keep track of quantum of time)
+	  	if(CpuIdling == false) {
+			jobTable.get(current).updateCurrentTime(p[5], timeSpentCpu);
+			System.out.println("CurrentTime = " + jobTable.get(current).getCurrentTime());
+		}
+	  
+	  	//go back to schedule the job to run
+	  	FirstComeFirstServe(a, p);
 	}
 	
 	/* SUPERVISOR CALL FROM USER PROGRAM */
 	public static void Svc(int[] a, int[] p) {
-      System.out.println("Currently at SVC job wants to: "+a[0]);
-      
-      //I know job wants to do IO from the print,
-      // so i just made a conditional statement to see if it works
-      if(a[0]==6) {
-         sos.siodisk(jobTable.get(current).getJobNum());
-         FirstComeFirstServe(a, p);
-         }       
+      		System.out.println("Currently at SVC job wants to: " + a[0]);
+
+	  	//if our idling flag is false we want to substract 
+	  	//time spent using cpu to the current time and add it
+	  	//to the job's running time
+	  	//(this is how we keep track of quantum of time)
+	  	if(CpuIdling == false) {
+			jobTable.get(current).updateCurrentTime(p[5], timeSpentCpu);
+			System.out.println("CurrentTime = " + jobTable.get(current).getCurrentTime());
+		}
+	  
+	  	//job wishes to terminate, so we increment the current job #
+	  	//and set the idling flag back to true
+	  	if(a[0] == 5) {
+			current++;
+			a[0] = 1;
+			CpuIdling = true;
+			System.out.println("JOB# " + current + " HAS TERMINATED\n");
+		}
+	  	//job wants to do io, so we call siodisk
+	  	//then go back to scheduler
+      		if(a[0] == 6) {
+        		sos.siodisk(jobTable.get(current).getJobNum());
+        		FirstComeFirstServe(a, p);
+        	}   
+	  	//job wants to block
+	 	//since at the moment we know theres no pending io
+	  	//we just set idling flag to true
+	  	if(a[0] == 7) {
+			a[0] = 1;
+			CpuIdling = true;
+		}
 	}	
 
 }
+
