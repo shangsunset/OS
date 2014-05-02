d5c67644f135c9c7f71710d13bcb6ed8ef667d51
manuelleung <manuelleung93@gmail.com>
2014-05-02 15:58:11 -0400

    fixing memory manager + added comments

--- OS/memoryManager.java
+++ OS/memoryManager.java
@@ -1,52 +1,73 @@
 import java.util.*;
 
 class MemoryManager {
-	public ArrayList<int[]> memory = new ArrayList<int[]>();
-	public ListIterator<int[]> i;
-	public int maxMem;
-	public int item[];
-	public boolean free = false;
+	public ArrayList<int[]> memory; //declare list for memory
+	public ListIterator<int[]> i; //declare iterator to traverse memory
+	public int max[] = {0, 100};
+	public int space[]; // only index [0] and [1] are going to be used
+	public int free;
 	
 	public MemoryManager() {
-		for(int j=0; j<100; j++)
-			memory.add(j);
-		i = memory.listIterator();
+		memory = new ArrayList<int[]>(); //initialize memory
+		i = memory.listIterator(); //set iterator to memory
+		memory.add(max); //initializes with arrays start at 0 and end 100
 	}
 	
-	public int freeSpace(int size) {
-		int available;
+	
+	public int freeSpace(int jobSize) {
+		//loop if there are jobs in the list to check or until we find
+		//a free space to place a job in
 		while(i.hasNext()==true) {
-			item = i.next();
-			if(item[1] > size) {
-				available = item[0];
-				item[0] = item[0]+size;
-				item[1] = item[1]-size;
-				return available;
+			space = i.next();
+			//if free space is greater than the size of job
+			if(space[1] > jobSize) { 
+				available = space[0];
+				//item[0] is end pointer of job
+				//eg. for job#1  occupied 18
+				space[0] = space[0]+jobSize;
+				//is available space
+				//available space 82
+				space[1] = space[1]-jobSize;
+				//return the available free space to place a job in
+				return free;
 			}
 		}
+		//return when No free space available
 		return -1;
 	}
 	
-	public void removeJob(int jobSize, int jobAddress) {
-		while(i.hasNext() == true) {
-			if(free == false) {
-				item = i.next();
-				if(item[0] > jobAddress) {
-					i.previous();
-					int newItem[] = {jobAddress, size};
-					free = true;
-				}
-			else 
+	public void remove(int jobSize, int jobAddress) {
+		//jobAddress is the start of the job to be deleted
+		while(true) {
+			space = i.next();
+			//space[0] gives us the first job found with endpointer 
+			//greater than the the starting address of the job to be deleted
+			if(space[0] > jobAddress) {
+				//free up the space
+				i.previous();
+				int newSpace[] = {jobAddress, size};
+				i.add(nextSpace);
+				//space in the table has been freed
+				//we exit the loop
 				break;
 			}
 		}
-		prevItem = null;
+		
+		//holds the job spaces to be erased/merged
+		int prevSpace[] = i.next();
 		while(i.hasNext() == true) {
-			if(prevItem == null) 
-				prevItem = i.next();
-			item = i.next();
-			if(prevItem[0] + prevItem[1] == item[0]) {
-				prevItem[1] = prevItem[1]+item[1];
+			//space holds each job space in memory
+			//eg job#1 = space 82
+			space = i.next();
+			//when space holds the memory space next to the previous
+			//job that is free, we merge them together
+			//and remove it.
+			//eg. job#1 prevSpace[0] = 0 , prevSpace[1] = 18
+			//the free space available next to it is space[0]
+			//which starts at 18, total free space[1] = 82
+			//so after adding we get 100 free space back
+			if(prevSpace[0] + prevSpace[1] == space[0]) {
+				prevSpace[1] = prevSpace[1]+space[1];
 				i.remove();
 			}
 		}
