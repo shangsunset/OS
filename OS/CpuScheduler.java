527394b0b3e0bd30c19e26aae0bbd51807e53e3e
manuelleung <manuelleung93@gmail.com>
2014-05-08 23:39:03 -0400

    added comments and some changes

--- OS/CpuScheduler.java
+++ OS/CpuScheduler.java
@@ -16,9 +16,7 @@ public class CpuScheduler {
 			for(int i =0; i<jobTable.size(); i++) {
 				//check until we find a job to run or for loop ends
 				if(jobTable.get(i)==runJob || jobTable.get(i).isBlocked() || jobTable.get(i).isFinished()){
-					//jobs that meet any of these conditions wont run on cpu - skip
-					continue;
-				else
+					//jobs that meet any of these conditions wont run on cpu
 					//as soon as we find a job that can run we exit loop
 					runJob = jobTable.get(i);
 					break;
@@ -31,17 +29,20 @@ public class CpuScheduler {
 			return new Job();
 		}
 		
+		//dont allow jobs that have reached its maxcputime to run
 		if(!(runJob.getMaxCpuTime()==runJob.getCpuTimeUsed())) {
-			if(runJob.getMaxCpuTime() < timeQuantum + runJob.getCurrentTime() && !runJob.isBlocked() && runJob.isInMemory()) {
-				//run job and return the running job to os
+			//if the time quantum + the time used is more than the maximum allowed
+			if(runJob.getMaxCpuTime() < timeQuantum+runJob.getCurrentTime() && !runJob.isBlocked() && runJob.isInMemory()) {
+				//run for remaining time
 				a[0]=2;
 				p[2]=runJob.getJobAddress();
 				p[3]=runJob.getJobSize();
 				p[4]=runJob.getMaxCpuTime - runJob.getCpuTimeUsed();
 				return runJob;
 			}
-			else if(runJob.getMaxCpuTime() >= timeQuantum && !runJob.isBlocked() && runJob.isInMemory()) {
-				//run job and return the running job to os
+			//else if the job maximum time is still greater than time quantum + time used
+			else if(runJob.getMaxCpuTime() >= timeQuantum+runJob.getCurrentTime() && !runJob.isBlocked() && runJob.isInMemory()) {
+				//run for time quantum
 				a[0]=2;
 				p[2]=runJob.getJobAddress();
 				p[3]=runJob.getJobSize();
